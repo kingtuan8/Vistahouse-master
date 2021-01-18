de11b6bf67c3f1e561e5f783b7e1ee161b13e9da
Tuan Bui <kingtuan8@gmail.com>
2021-01-18 19:46:10 +0700

    tom

--- NhomXingfa/Controllers/HomeController.cs
+++ NhomXingfa/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using NhomXingfa.Areas.Quantri.Models.DataModels;
+using NhomXingfa.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +10,13 @@ namespace NhomXingfa.Controllers
 {
     public class HomeController : Controller
     {
+        XingFaEntities db = new XingFaEntities();
         public ActionResult Index()
         {
-            return View();
+            var model = new IndexPageViewModel();
+            model.goidinhky = db.Products.Where(q => q.IsActive == true && q.IsProduct == false).ToList();
+            model.products = db.Products.Where(q => q.IsActive == true && q.IsProduct == true).ToList();
+            return View(model);
         }
 
         public ActionResult About()
